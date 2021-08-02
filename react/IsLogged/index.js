e0559ed4cac34258887f7cdf7f87054b193f31e9
Jean Nussenzveig <jean@veig.com.br>
2021-08-02 14:56:16 -0300

    Fixed sessions parameters

--- react/IsLogged/index.js
+++ react/IsLogged/index.js
@@ -13,7 +13,7 @@ const IsLogged = () => {
 
   const fetchUser = async () => {
     const session = (await axios({
-      url: '/api/sessions?items=*',
+      url: '/api/sessions?items=account.id,account.accountName,store.channel,store.countryCode,store.cultureInfo,store.currencyCode,store.currencySymbol,store.admin_cultureInfo,creditControl.creditAccounts,creditControl.deadlines,creditControl.minimumInstallmentValue,authentication.storeUserId,authentication.storeUserEmail,profile.firstName,profile.document,profile.email,profile.id,profile.isAuthenticated,profile.lastName,profile.phone,public.favoritePickup,public.utm_source,public.utm_medium,public.utm_campaign,public.utmi_cp,public.utmi_p,public.utmi_pc',
       method: 'GET'
     })).data
 
