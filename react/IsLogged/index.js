de9e33eda2005af8cd2dc2be6679e3f20e651b29
Jean Nussenzveig <jean@veig.com.br>
2021-08-02 15:35:34 -0300

    Fix URL

--- react/IsLogged/index.js
+++ react/IsLogged/index.js
@@ -13,7 +13,7 @@ const IsLogged = () => {
 
   const fetchUser = async () => {
     const session = (await axios({
-      url: (window.location.host === 'www.ido.it' ? window.location.pathname.split("/")[1] : '') + '/api/sessions?items=account.id,account.accountName,store.channel,store.countryCode,store.cultureInfo,store.currencyCode,store.currencySymbol,store.admin_cultureInfo,creditControl.creditAccounts,creditControl.deadlines,creditControl.minimumInstallmentValue,authentication.storeUserId,authentication.storeUserEmail,profile.firstName,profile.document,profile.email,profile.id,profile.isAuthenticated,profile.lastName,profile.phone,public.favoritePickup,public.utm_source,public.utm_medium,public.utm_campaign,public.utmi_cp,public.utmi_p,public.utmi_pc',
+      url: (window.location.host === 'www.ido.it' ? '/' + window.location.pathname.split("/")[1] : '') + '/api/sessions?items=account.id,account.accountName,store.channel,store.countryCode,store.cultureInfo,store.currencyCode,store.currencySymbol,store.admin_cultureInfo,creditControl.creditAccounts,creditControl.deadlines,creditControl.minimumInstallmentValue,authentication.storeUserId,authentication.storeUserEmail,profile.firstName,profile.document,profile.email,profile.id,profile.isAuthenticated,profile.lastName,profile.phone,public.favoritePickup,public.utm_source,public.utm_medium,public.utm_campaign,public.utmi_cp,public.utmi_p,public.utmi_pc',
       method: 'GET'
     })).data
 
